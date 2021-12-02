9a6483c6120a7b71b0d927339f2d7dbdae61ce0d
admin <admin@49292.local>
2021-12-02 13:19:26 +0000

    Done

--- Cold Call/Cold Call/ViewController.swift
+++ Cold Call/Cold Call/ViewController.swift
@@ -38,17 +38,7 @@ class ViewController: UIViewController {
 
     @IBAction func btnClicked(_ sender: Any) {
         nameLable.text = names[Int.random(in: 0...(names.count - 1))]
-        let randomNumber = Int.random(in: 1...5)
-        if randomNumber == 1 || randomNumber == 2{
-            nummber.textColor = UIColor.red
-        }
-        else if randomNumber == 3 || randomNumber == 4{
-            nummber.textColor = UIColor.orange
-        }
-        else{
-            nummber.textColor = UIColor.green
-        }
-        nummber.text = String(randomNumber)
+        
     }
 }
 
