bb26aea1c26f79986afa6a2d8db7e00ca491541f
admin <admin@49292.local>
2021-12-02 13:05:19 +0000

    Done

--- Cold Call/Cold Call/ViewController.swift
+++ Cold Call/Cold Call/ViewController.swift
@@ -27,17 +27,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nameLable: UILabel!
     
+
+    @IBOutlet weak var nummber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        nameLable.text = names[Int.random(in: 0...(names.count - 1))]
-        
     }
 
 
     @IBAction func btnClicked(_ sender: Any) {
         nameLable.text = names[Int.random(in: 0...(names.count - 1))]
+        let randomNumber = Int.random(in: 1...5)
+        if randomNumber == 1 || randomNumber == 2{
+            nummber.textColor = UIColor.red
+        }
+        else if randomNumber == 3 || randomNumber == 4{
+            nummber.textColor = UIColor.orange
+        }
+        else{
+            nummber.textColor = UIColor.green
+        }
+        nummber.text = String(randomNumber)
     }
 }
 
